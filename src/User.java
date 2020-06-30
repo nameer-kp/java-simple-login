1bf151dae2936a443ad205058c53da1129a0e353
Nameer-kp <kp2nameer@yahoo.com>
2020-07-01 01:51:49 +0530

    features added

--- src/User.java
+++ src/User.java
@@ -9,16 +9,24 @@ import java.sql.Statement;
 public class User {
     private String username;
     private String password;
-    private int repeat=1;
+    private char repeat='y';
+    private int choose;
 
 
     public User() throws SQLException {
-        while (repeat == 1) {
+        while (repeat == 'y') {
             Scanner scanner = new Scanner(System.in);
             System.out.printf("Enter UserName:");
             username = scanner.nextLine();
             System.out.printf("Enter Password:");
             password = scanner.nextLine();
+            System.out.println("1.for creating account \n 2.for login to account");
+            // option for login or creating
+            choose = scanner.nextInt();
+
+            //used to move scanner to next line
+            scanner.nextLine();
+
 
             try {
                 // postgres Driver registering
@@ -31,21 +39,31 @@ public class User {
                 String query2 = "INSERT INTO public.login(\n"+
                         "username, password)\n" +
                         "\tVALUES ('"+username+"','"+password+"');";
-
+                if (choose==2){         //login code
                 ResultSet rs = statement.executeQuery(query);
                 if (rs.next()) {
                     System.out.println("Login Successful");
                 } else {
+                    System.out.println("Creating new account"); //creating account if doesnt exist on database
                     statement.executeUpdate(query2);            // using executeUpdate since were not performing selection query
                     System.out.println("Account Created");
-                }
+                        }
                 statement.close();
                 connection.close();
-            } catch (Exception e) {
+                }
+                else if (choose ==1){       //account creation code
+                    System.out.println("user choose account creation mode");
+                    statement.executeUpdate(query2);            // using executeUpdate since were not performing selection query
+                    System.out.println("Account Created");
+                    statement.close();
+                    connection.close();
+                }
+            }
+                catch (Exception e) {
                 e.printStackTrace();
             }
-            System.out.printf("Do you want to repeat:");
-            this.repeat = scanner.nextInt();
+            System.out.println("Do you want to repeat:");
+            this.repeat = scanner.nextLine().charAt(0);
         }
     }
 }
